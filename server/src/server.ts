e2abe2e7f9a8d14fb3b47133fee2f894ca26fe4f
joeperpetua <qwerty.perpetua@gmail.com>
2024-09-16 00:33:10 +0200

    Implement energy regen and action handling. Also some connection behaviors

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ interface ClientMessage {
     type: "searchGame" | "requestSpin" | "sendAction";
     matchId: string | null;
     playerId: string;
-    action: "Advance" | "Attack" | "Defend" | "Energize" | null;
+    actions: SpinItem[];
 }
 
 interface SpinItem {
@@ -47,7 +47,8 @@ console.log('running on port 8080');
 // WebSocket event handling
 wss.on("connection", (ws) => {
     let currentMatchId: string | null = null;
-    let currentPlayerId: string;
+    let currentPlayerId: string = '';
+    let currentRegenInterval: NodeJS.Timeout;
 
     // Event listener for incoming messages
     ws.on("message", (message) => {
@@ -76,6 +77,9 @@ wss.on("connection", (ws) => {
 					// Create room and send data to client
                     handleJoinGame(matchId, players[0].ws);
 					handleJoinGame(matchId, players[1].ws);
+
+                    // Create interval for energy regen
+                    currentRegenInterval = initEnergyRegen(matchId, [players[0].playerId, players[1].playerId]);
                 } else {
                     // If no player waiting, add current player to waitlist
                     lookingForGame.push({ playerId: data.playerId, ws: ws });
@@ -94,20 +98,28 @@ wss.on("connection", (ws) => {
                 
                 break;
 
-            // case "sendAction":
-            //     handleSendAction(ws, data.matchId, data.playerId, data.action);
-            //     break;
+            case "sendAction":
+                if (!data.matchId || !data.actions) {
+                    ws.send(JSON.stringify({ error: "matchID or action missing in message!" }));
+                    return;
+                }
+                handleSendAction(ws, data.matchId, data.playerId, data.actions);
+                break;
 
             default:
                 ws.send(JSON.stringify({ error: "Unknown message type" }));
         }
     });
 
-	// Remove ws from match room if the client disconnects
+	// Remove ws from match room / waiting list if the client disconnects
     ws.on('close', () => {
 		if (currentMatchId && matchRooms[currentMatchId]) {
-		  matchRooms[currentMatchId].delete(ws);
-          console.log(`[LOG] [SERVER] [MatchRoom}] [${currentPlayerId}] :: ${new Date().toISOString()} = Removed player match room`);
+            broadcastMessage(currentMatchId, {
+                type: 'adversaryDisconnection',
+                matchId: currentMatchId
+            });
+            matchRooms[currentMatchId].clear();
+            console.log(`[LOG] [SERVER] [MatchRoom}] [${currentMatchId}] :: ${new Date().toISOString()} = ${currentPlayerId} disconnected. Removed both players from match room`);
 		}
 
         // Handle connection closed while waiting for match
@@ -118,8 +130,47 @@ wss.on("connection", (ws) => {
                 console.log(`[LOG] [SERVER] [LookingForMatch}] [${currentPlayerId}] :: ${new Date().toISOString()} = Removed player from wait list`);
             }
         }
+
+        if (currentRegenInterval) {
+            clearInterval(currentRegenInterval);
+        }
 	});
 });
+
+function initEnergyRegen(matchId: string, playersId: string[]) {
+    const regenInterval = setInterval(() => {
+        let regenTriggered = false;
+        let gameState = gameService.getMatchState(matchId);
+
+        playersId.forEach(playerId => {
+            if (gameState.players[playerId].energy < 10) { // Only trigger regen if energy is below 10
+                commandHandler.playerEnergize(matchId, playerId, 1);
+                regenTriggered = true;
+            }
+        });
+
+        if (regenTriggered) {
+            gameState = gameService.getMatchState(matchId); // Update state before sending
+            broadcastMessage(matchId, {
+                type: 'updateGameState',
+                matchId: matchId,
+                state: gameState
+            });
+        }
+    }, 5000);   
+    return regenInterval;
+}
+
+function broadcastMessage(matchId: string, message: {}) {
+    const matchRoom = matchRooms[matchId];
+    for (const clientWs of matchRoom) {
+        if (clientWs.readyState === WebSocket.OPEN) {
+            clientWs.send(
+                JSON.stringify(message)
+            );
+        }
+    }
+}
 
 // Handle joining a game
 function handleJoinGame(matchId: string, ws: WebSocket) {
@@ -177,4 +228,35 @@ function handleRequestSpin(ws: WebSocket, matchId: string, playerId: string) {
     }
 }
 
-function handleSendAction(ws: WebSocket, matchId: string, playerId: string) {}
+function handleSendAction(ws: WebSocket, matchId: string, playerId: string, actions: SpinItem[]) {
+    let gameState = gameService.getMatchState(matchId);
+    const adversaryPlayerId = Object.keys(gameState.players).find(player => player != playerId);
+
+    actions.forEach(action => {
+        switch(action.type) {
+            case 'Advance':
+                commandHandler.playerAdvance(matchId, playerId, action.value, 0);
+                break;
+            case 'Attack':
+                if (adversaryPlayerId)
+                    commandHandler.playerAttack(matchId, playerId, adversaryPlayerId, action.value, 0);
+                break;
+            case 'Defend':
+                commandHandler.playerDefend(matchId, playerId, action.value, 0);
+                break;
+            case 'Energize':
+                commandHandler.playerEnergize(matchId, playerId, action.value);
+                break;
+            default:
+                break;
+        }
+    });
+
+    gameState = gameService.getMatchState(matchId);
+
+    broadcastMessage(matchId, {
+        type: 'updateGameState',
+        matchId: matchId,
+        state: gameState
+    });
+}
