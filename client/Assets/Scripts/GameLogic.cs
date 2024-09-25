6c212cfd3a4b5adbb4522e9856c98b7db1738c54
joeperpetua <qwerty.perpetua@gmail.com>
2024-09-25 20:50:13 +0200

    Add WebSocket connection. Move POC client to root folder

--- client/Assets/Scripts/GameLogic.cs
+++ client/Assets/Scripts/GameLogic.cs
@@ -11,7 +11,10 @@ public class GameLogic : MonoBehaviour
     public bool gameStarted = false;
     public bool gameOver = false;
     public int winner;
+    [SerializeField] private string serverAddress = "ws://localhost:8080";
     [SerializeField] private float gameDuration = 15;
+
+    private WebSocket _websocket;
 
     public void startGame(int designatedWinner)
     {
@@ -24,4 +27,34 @@ public class GameLogic : MonoBehaviour
     {
         gameOver = true;
     }
+
+    private async void Start()
+    {
+        _websocket = new WebSocket(serverAddress);
+        _websocket.OnOpen += () =>
+        {
+            Debug.Log("Connection open!");
+        };
+
+        _websocket.OnError += (e) =>
+        {
+            Debug.Log("Error! " + e);
+        };
+
+        _websocket.OnClose += (e) =>
+        {
+            Debug.Log("Connection closed!");
+        };
+
+        _websocket.OnMessage += (bytes) =>
+        {
+            Debug.Log("OnMessage!");
+            string message = System.Text.Encoding.UTF8.GetString(bytes);
+            Debug.Log(message);
+        };
+
+        // waiting for messages
+        await _websocket.Connect();
+    }
+
 }
